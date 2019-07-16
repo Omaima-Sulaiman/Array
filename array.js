daa86530db2cbc7d9b9bfeec05461defd022e780
omaima <omemah.sh@gmail.com>
2019-07-16 15:26:19 +0300

    solve

--- array.js
+++ array.js
@@ -5,11 +5,11 @@ console.log('Here is : ', 'array')
 /*
 1
 Correct the syntax error
- [ 1,7  9  45, ]
+ [ 1,7 , 9 , 45, ]
 
- ["Str" "alex","moh"
+ ["Str","alex","moh",
 
- 'the','fox' 'over' lazy, 'dog',  ]
+ 'the','fox' ,'over', 'lazy', 'dog'  ]
 
 */
 
@@ -18,7 +18,7 @@ Correct the syntax error
 2
 What the index of "Banana","Tomato"
 var fruits=["Tomato","Banana","Watermelon"]
-
+// banana = 1 and tomato =0
 */
 
 
@@ -29,7 +29,9 @@ Create an array represents your:
 2- Favorite Sport (3)
 3- Favorite Movie (4)
 */
-
+var arrFood = ["mansaf", "mlu5ia", "ma8loba", "dawali", "dolma"]
+var arrSport = ["football", "running", "hiking"]
+var arrMovie = ["dontBreath", "wonder", "IT", "gone girls"]
 
 /*
 4
@@ -41,6 +43,9 @@ Ex: firstOfArray([1,4,5]) => 1
 Ex: firstOfArray(["t","u","g","x"]) => "t"
 */
 
+function firstOfArray(arr) {
+    return arr[0]
+}
 
 /*
 5
@@ -51,7 +56,9 @@ and return the first element in an array
 Ex: lastOfArray([1,4,5]) => 5
 Ex: lastOfArray(["t","u","g","x"]) => "x"
 */
-
+function lastOfArray(arr) {
+    return arr[arr.length - 1]
+}
 
 /*
 6
@@ -60,13 +67,25 @@ Using console make this array to be like this one (push, unshift, shift, pop)
 var array = [0,5,7,9]
 => [1,3,4,6,8,9,10]
 */
-
-
+var arr = [0, 5, 7, 9]
+console.log(arr)
+arr.shift()
+arr.shift()
+arr.shift()
+arr.unshift(8)
+arr.unshift(6)
+arr.unshift(4)
+arr.unshift(3)
+arr.unshift(1)
+arr.push(10)
+console.log(arr)
 /*
 7
 Using the console try to figure out what the thing thats (push, unshift, shift, pop) return to you
 
 var array2 = [5,9,-7,3.5]
+
+done
 */
 
 
@@ -80,7 +99,19 @@ and return the two middle elemnt in an array if it is have an even elemnets
 Ex: middleOfArray([1,4,5]) => 4
 Ex: middleOfArray(["t","u","g","x"]) =>"u and g"
 */
+function middleOfArray(arr) {
+    var t
+    if (arr.length % 2 === 0) {
 
+        t = arr[((arr.length / 2) - 1)] + " and " + arr[(arr.length / 2)]
+        return t
+    }
+    else {
+
+        t = arr[(((arr.length - 1) / 2))]
+        return t
+    }
+}
 
 /*
 9
@@ -94,7 +125,23 @@ animals; => ['zebra', 'elephant']
 var nums= [1,2,3,8,9]
 nums => [5,-22,3.5,44,98,44]
 */
+var animals = ['cat', 'ele', 'bird']
+console.log(animals)
+animals[0] = 'zebra'
+animals[1] = 'elephant'
+animals.length = 2
+console.log(animals)
 
+var nums = [1, 2, 3, 8, 9]
+console.log(nums)
+
+nums[0] = 5
+nums[1] = +22
+nums[2] = 3.5
+nums[3] = 44
+nums[4] = 98
+nums[5] = 44
+console.log(nums)
 
 /*
 10
@@ -109,7 +156,10 @@ Ex: indexOfArray(nums,4) => 9
 
 **try more cases by your self
 */
+function indexOfArray(arr, n) {
 
+    return arr[n]
+}
 
 /*
 11
@@ -122,7 +172,11 @@ Ex: arrayExceptLast(nums) =>  [1,2,3,8]
 
 **try more cases by your self
 */
+function arrayExceptLast(arr) {
+    arr.length = arr.length - 1
+    return arr
 
+}
 
 /*
 12
@@ -135,7 +189,12 @@ Ex: addToEnd(nums,55) =>  [1,2,3,8,55]
 
 **try more cases by your self
 */
+function addToEnd(arr, val) {
 
+    arr[arr.length - 1] = val
+
+    return arr
+}
 
 // all the exercises below should solved 2 times: 1- for loop 2- while lopp
 /*
@@ -150,6 +209,33 @@ Ex: sumArray(nums) => 23
 ** solve it one time using for loop and another using while loop
 **try more cases by your self
 */
+function sumArrayw(arr) {
+    var sum = 0;
+
+    while (arr.length !== 0) {
+        sum += arr[(arr.length - 1)]
+
+        arr.length--
+    }
+    return sum
+}
+
+
+
+
+function sumArrayf(arr) {
+    var sum = 0;
+
+
+
+    for (let i = 0; i !== arr.length; i++) {
+        sum += arr[i]
+
+    }
+
+    return sum
+}
+
 
 
 /*
@@ -164,6 +250,38 @@ Ex: minInArray(nums) => 1
 ** solve it one time using for loop and another using while loop
 **try more cases by your self
 */
+// function minInArrayW(arr) {
+//     var min = arr[arr.length - 1]
+//     while (arr.length !== 0) {
+//         if (arr[arr.length - 1] <= min) {
+//             min = arr[arr.length - 1]
+//         }
+//         arr.length--
+//     }
+//     return min
+// }
+
+
+function minInArrayW(arr) {
+    while (arr.length !== 1) {
+        if (arr[arr.length - 1] <= arr[arr.length - 2]) {
+            arr[arr.length - 2] = arr[arr.length - 1]
+        }
+        arr.length--
+    }
+    return arr
+}
+
+function minInArrayF(arr) {
+    for (let i = 0; i !== arr.length; i++) {
+        if (arr[i] <= arr[i + 1]) {
+            arr[i + 1] = arr[i]
+        }
+    }
+    return arr[arr.length - 1]
+}
+
+
 
 
 /*
@@ -178,7 +296,34 @@ Ex: minInArray(nums,8) => [1,2,3,9]
 ** solve it one time using for loop and another using while loop
 **try more cases by your self
 */
+function removeFromArray(arr, n) {
+    var i = 0
+    while (i <= (arr.length - 1)) {
 
+        if (n === arr[i]) {
+
+            arr.splice(i, 1);
+            i--
+        }
+        i++
+    }
+
+    return arr
+}
+
+
+function removeFromArrayF(arr, n) {
+    for (let i = 0; i < arr.length; i++) {
+
+        if (n === arr[i]) {
+
+            arr.splice(i, 1);
+            i--
+        }
+    }
+
+    return arr
+}
 
 /*
 16
@@ -192,7 +337,33 @@ Ex: oddArray(nums) => [1,3,9]
 ** solve it one time using for loop and another using while loop
 **try more cases by your self
 */
+function oddArray(arr) {
+    var i = 0
+    while (i <= (arr.length - 1)) {
 
+        if (arr[i] % 2 === 0) {
+
+            arr.splice(i, 1);
+            i--
+        }
+        i++
+    }
+
+    return arr
+}
+
+
+function oddArrayf(arr) {
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] % 2 === 0) {
+            arr.splice(i, 1);
+            i--
+        }
+    }
+
+    return arr
+}
 
 /*
 17
@@ -211,11 +382,40 @@ Ex: aveArray(nums) => 16.6
 */
 
 
+function aveArray(arr) {
+    var i = 0
+    while (i <= (arr.length - 1)) {
+
+        if (arr[i] % 2 === 1) {
+
+            arr.splice(i, 1);
+            i--
+        }
+        i++
+    }
+
+    return arr
+}
+
+
+function aveArrayf(arr) {
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] % 2 === 1) {
+            arr.splice(i, 1);
+            i--
+        }
+    }
+
+    return arr
+}
+
+
 /*
 18
 Create a function called shorterInArray
 that accept an array of strings
-and return the shortest string inside this array (first)
+and  returnthe shortest string inside this array (first)
 
 var strings= ["alex","mercer","madrasa","rashed2","emad","hala"]
 Ex: shorterInArray(strings) => "alex"
@@ -224,7 +424,29 @@ Ex: shorterInArray(strings) => "alex"
 **try more cases by your self
 */
 
+function shorterInArray(arr) {
+    var min = arr[0]
+    var i = 0
+    while (arr.length !== i) {
+        if (arr[i].length <= min.length) {
+            min = arr[i]
+        }
+        i++
+    }
+    return min
+}
 
+
+
+function shorterInArrayf(arr) {
+    var min = arr[0]
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].length <= min.length) {
+            min = arr[i]
+        }
+    }
+    return min
+}
 /*
 19
 Create a function called repeatChar
